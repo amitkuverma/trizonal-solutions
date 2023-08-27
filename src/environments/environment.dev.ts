8db037450dc5250ed1931c43ae335e4a7c2d160d
AMIT VERMA <amit.verma@softobotics.com>
2023-08-27 10:27:57 +0530

    blank

--- src/environments/environment.dev.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    API_URL: 'https://opusapi.dev.laneapp.se/api',
-    OLD_LOGIN_URL: 'https://testcentral.dev.laneapp.se/TestCentralDev/InspectorPortal/Login.aspx',
-    envName: 'dev',
-    envColor: '#20b2aa',
-}
--- src/environments/environment.qa.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    API_URL: 'https://opusapi.qa.laneapp.se/api',
-    OLD_LOGIN_URL: 'https://testcentral.qa.laneapp.se/TestCentralQA/InspectorPortal/Login.aspx',
-    envName: "qa",
-    envColor: '#add8e6',
-}
