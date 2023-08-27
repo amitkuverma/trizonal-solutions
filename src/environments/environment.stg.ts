e9e5b7721a8ad98ed98e747c7739f49de8d6a82a
AMIT VERMA <amit.verma@softobotics.com>
2023-08-27 09:59:44 +0530

    three

---
+++ src/environments/environment.stg.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    API_URL: 'https://opusapi.stg.laneapp.se/api',
+    OLD_LOGIN_URL: 'https://testcentral.stg.laneapp.se/TestCentralUAT/InspectorPortal/Login.aspx',
+    envName: "uat",
+    envColor: '#90ee90',
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    API_URL: 'https://opusapi.dev.laneapp.se/api',
+    envName: "local",
+    OLD_LOGIN_URL: 'http://localhost/testCentral/InspectorPortal/login.aspx',
+    envColor: '#2082b2',
+}
