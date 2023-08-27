8ab21e47e154364b29583efceb38f556d6e30bbc
AMIT VERMA <amit.verma@softobotics.com>
2023-08-27 19:50:12 +0530

    link

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,34 +1,35 @@
 import React, { useState } from 'react';
 import "../assets/styles/layout/header.scss"
 import logo from "../assets/images/logo.png"
+import { Link, useNavigate } from 'react-router-dom'
 
 export const Header = () => {
-  return (  
+  return (
     <nav className="navbar navbar-expand-sm sticky-top header">
       <div className="container ">
-        <a href='/' className="navbar-brand pointer logo">
+        <Link to={"/"} className="navbar-brand pointer logo">
           <img src={logo} alt='logo' />
           Trizonal <span className="sub">Solutions</span>
-        </a>
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-         <ul className="navbar-nav">
-             <li className='nav-item'><a href='/' className="nav-link active text-uppercase pointer text-decoration-none">Home</a></li>
-             <li className='nav-item'><a href='/about' className="nav-link text-uppercase pointer text-decoration-none">About</a></li>
-             <li className='nav-item'><a className="nav-link text-uppercase pointer text-decoration-none service pe-3">Service</a>
-               <div className='sub-menu'>
-                 <ul className='ps-0 py-4 bg-white'>
-                   <li><a href='/hospitals-providers' className="text-uppercase pointer text-decoration-none">Hospitals & Providers</a></li>
-                   <li><a href='/payer-services' className="text-uppercase pointer text-decoration-none">Payer Services</a></li>
-                 </ul>
-               </div>
-             </li>
-             <li className='nav-item'><a href='/our-clients' className="nav-link text-uppercase pointer text-decoration-none">Our Clients</a></li>
-             <li className='nav-item'><a href='/blogs' className="nav-link text-uppercase pointer text-decoration-none">Blogs</a></li>
-             <li className='nav-item'><a href='/contact' className="nav-link text-uppercase pointer text-decoration-none">Contact</a></li>
-           </ul>
+          <ul className="navbar-nav">
+            <li className='nav-item'><Link to={"/"} className="nav-link active text-uppercase pointer text-decoration-none">Home</Link></li>
+            <li className='nav-item'><Link to={"/about"} className="nav-link text-uppercase pointer text-decoration-none">About</Link></li>
+            <li className='nav-item'><Link className="nav-link text-uppercase pointer text-decoration-none service pe-3">Service</Link>
+              <div className='sub-menu'>
+                <ul className='ps-0 py-4 bg-white'>
+                  <li><Link to={"/hospitals-providers"} className="text-uppercase pointer text-decoration-none">Hospitals & Providers</Link></li>
+                  <li><Link to={"/payer-services"} className="text-uppercase pointer text-decoration-none">Payer Services</Link></li>
+                </ul>
+              </div>
+            </li>
+            <li className='nav-item'><Link to={"/our-clients"} className="nav-link text-uppercase pointer text-decoration-none">Our Clients</Link></li>
+            <li className='nav-item'><Link to={"/blogs"} className="nav-link text-uppercase pointer text-decoration-none">Blogs</Link></li>
+            <li className='nav-item'><Link to={"/contact"} className="nav-link text-uppercase pointer text-decoration-none">Contact</Link></li>
+          </ul>
         </div>
       </div>
     </nav>
