c28a826848f710018ec1407e7b1a60988b450b22
AMIT VERMA <amit.verma@softobotics.com>
2023-08-27 19:06:16 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './components/home';
 import { About } from './components/about';
 import { PayerServices } from './components/payer-services';
