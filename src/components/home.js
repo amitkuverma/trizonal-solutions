4e5abd693424dbcc928d2aa12ef313ed8b1eab91
AMIT VERMA <amit.verma@softobotics.com>
2023-09-04 17:37:33 +0530

    text update

--- src/components/home.js
+++ src/components/home.js
@@ -12,7 +12,7 @@ export const Home = () => {
             <div className='container'>
               <div className='health'>
                 <div className='transparent p-3 p-md-4 p-lg-5 animated slideInUp'>
-                  <div className='text-01 text-white fnt-36 fnt-md-56'>HEALTHALL <br /> CONSULTING</div>
+                  <div className='text-01 text-white fnt-36 fnt-md-56'>TRIZONAL <br /> SOLUTIONS</div>
                   <div className='text-02 text-white fnt-20 fnt-md-30'>Your trusted business partner for <br /> Hospitals and Payers.</div>
                   <p className='text-white fnt-16 d-none d-md-block'>Contact us for quality and value you can trust.</p>
                   <button className='btn get-btn btn-primary py-2 px-3'>Get in touch</button>
@@ -29,10 +29,10 @@ export const Home = () => {
                 <div className="">
                   <div className="">
                     <div className="">
-                      <h1><strong>Welcome to HealthAll Consulting</strong></h1>
-                      <p>HealthAll Consulting is a firm that is dedicated and focused entirely on the healthcare industry market with two key areas of focus – <strong>Providers </strong>and<strong> Payers</strong>.</p>
+                      <h1><strong>Welcome to Trizonal Solutions</strong></h1>
+                      <p>Trizonal Solution is a firm that is dedicated and focused entirely on the healthcare industry market with two key areas of focus – <strong>Providers </strong>and<strong> Payers</strong>.</p>
                       <p>Executives of the company are well connected to Subject Matter Experts and senior healthcare professionals that offer deep domain experience that will deliver optimal results in helping your organization meet its objectives.</p>
-                      <p>HealthAll Consulting’s service offerings will allow your organization to focus on what you do best, that is, improving the delivery of quality patient care and member services. Let us do what we do best, helping you reduce operating cost and maximize your investment in information technology.</p>
+                      <p>Trizonal Solution's service offerings will allow your organization to focus on what you do best, that is, improving the delivery of quality patient care and member services. Let us do what we do best, helping you reduce operating cost and maximize your investment in information technology.</p>
                     </div>
                   </div>
                   <div className="">
@@ -69,31 +69,6 @@ export const Home = () => {
                         </div>
                       </div>
                     </div>
-                    <div className="wpb_column vc_column_container col-sm-6">
-                      <div className="wpb_wrapper">
-                        <div className="teaser_box wpb_content_element text-center image_hover skincolored_section    with_button    ">
-
-                          {/* <div className="figure ">
-                            <Link to={"/contact"}>
-                              <div className="figure  stretchy_wrapper ratio_16-9" style={"background-image:url('https://healthallconsult.com/wp-content/uploads/2020/07/Depositphotos_57378019_xl-2015-scaled.jpg')"}></div>
-                            </Link>
-                          </div> */}
-
-                          {/* <div className="content text-center boxed with_button">
-                            <div className="hgroup">
-                              <h4 className="neutralize_links">
-                                CONTACT US TODAY
-                              </h4>
-                              <p>Speak to a professional!</p>
-                            </div>
-
-                            <div className="link centered">
-                              <a href="/contact" target="" className="btn btn-xs btn-secondary"><strong>More</strong></a>
-                            </div>
-                          </div> */}
-                        </div>
-                      </div>
-                    </div>
                   </div>
                 </div>
               </div>
@@ -115,7 +90,7 @@ export const Home = () => {
                       <img className='img-fluid' src='https://healthallconsult.com/wp-content/uploads/2020/07/Depositphotos_23068096_xl-2015-scaled.jpg' alt='' />
                       <div className='p-3 text-center'>
                         <div className='fw-bold py-2'>PROVIDERS</div>
-                        <p>HealthAll Consulting assists healthcare providers in using technology to increase performance, productivity, achieve clinical integration, optimization, improve patient care quality and wellness services.</p>
+                        <p>Trizonal Solution assists healthcare providers in using technology to increase performance, productivity, achieve clinical integration, optimization, improve patient care quality and wellness services.</p>
                         <button className='btn btn-primary br-0'>More</button>
                       </div>
                     </div>
@@ -148,9 +123,9 @@ export const Home = () => {
                 <div className='col-12 col-lg-4 ms-lg-3'>
                   <div className='text-white fnt-18 fw-900 fnt-fly fnt-18'>OUR PROMISE TO YOU</div>
                   <div>
-                    <p className='text-white'>At HealthAll Consulting, we provide “<strong>QUALITY</strong>” consulting that will meet the specific needs of your business.</p>
+                    <p className='text-white'>At Trizonal Solution, we provide “<strong>QUALITY</strong>” consulting that will meet the specific needs of your business.</p>
                     <p className='text-white'>We are committed to providing our clients with the “<strong>BEST VALUE</strong>” for their investment.</p>
-                    <p className='text-white'>HealthAll Consulting strives to create a lasting partnership with our clients based on “<strong>TRUST.</strong>”</p>
+                    <p className='text-white'>Trizonal Solution strives to create a lasting partnership with our clients based on “<strong>TRUST.</strong>”</p>
                   </div>
                 </div>
               </div>
