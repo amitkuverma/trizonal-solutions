b5b4b069e814888c9fc399778bc509c859ab8e57
AMIT VERMA <amit.verma@softobotics.com>
2023-09-06 16:16:12 +0530

    footer

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -9,13 +9,13 @@ export const Footer = () => {
       <div className='footer-sec bg-white'>
         <div className='container py-4 py-md-5'>
           <div className='row pt-5'>
-            <div className='col-12 col-md-6 pb-4 pb-md-0'>
+            <div className='col-12 col-lg-6 pb-4 pb-lg-0 text-center'>
               <Link to={"/"} className="navbar-brand pointer logo">
                 <img className='img-fluid' src={logo} alt='logo' />
                 <span className='fnt-24 fnt-md-36 fw-900 fnt-fly text-uppercase footer-text'>Trizonal <span className="txt-clrf9">Solutions</span></span>
               </Link>
             </div>
-            <div className='col-12 col-md-6'>
+            <div className='col-12 col-lg-6'>
               <div className='fnt-18 fnt-fly fw-900 text-uppercase pb-1 pb-md-4'>Trizonal Solutions</div>
               <p className='mb-1'><b>Email:</b> healthcare@trizonalsolutions.com</p>
               <p><b>Address:</b> Cigna Healthcare. New York, New York, United States</p>
